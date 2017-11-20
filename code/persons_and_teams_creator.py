14381cbd2bef642fb5d5409bc2ae9969566e1bd6
Nicolas Schmidt <nicolas.schmidt@heimspiel.de>
2017-11-20 12:33:17 +0100

    PersonsAndTeamsCreator() -> productive run: first 1000 profiles done

--- code/persons_and_teams_creator.py
+++ code/persons_and_teams_creator.py
@@ -123,7 +123,7 @@ class PersonsAndTeamsCreator():
 		profiles = csv_handler.CsvHandler().read_csv(self.profilesPath, 'r', 'latin-1', ',', '|', '2')
 		modifiedProfiles = []
 
-		for i in range(300,400):#(0,10)(10,20)
+		for i in range(1000,1200):#(0,10)(10,20)
 			if profiles[i][7] == '0':
 				teamId = self.create_new_person_and_team(profiles[i], adminUrl)
 				profiles[i][7] = teamId
